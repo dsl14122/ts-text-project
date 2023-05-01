df1b5c10110d66bbb96d8b74d8b409da05c1eacd
daishanglong <1596720698@qq.com>
2023-05-01 16:35:21 +0800

    feat: test

---
+++ test.ts
@@ -0,0 +1,15 @@
+const a: string= 'test'
+
+console.log(a);
+ 
+enum USER_RODE {
+    USER,
+    ORDER,
+}
+
+console.log(USER_RODE);
+
+const B = USER_RODE.ORDER;
+console.log(B);
+
+const ss: undefined|null =  null
